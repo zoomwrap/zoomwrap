e48b46d1b8bc64e271b6182beceb8aed5e90c5bc
WinWrap <kima4@elev.tec.dk>
2025-06-07 08:10:17 +0200

    constact

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
 
 
 export const routes: Routes = [
-
+{path: 'contact', component: ContactComponent}
 ];
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+import { routes } from './app/app.routes';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes),
+    provideHttpClient()
+  ]
+})
+  .catch(err => console.error(err));
