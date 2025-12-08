6963d940949a8ca95630a1ff6ebda61b35c330c3
Kim Lasse Massesson <kima4@elev.tec.dk>
2025-12-08 15:59:25 +0100

    newp

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 import { ContactComponent } from './contact/contact.component';
-
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-{path: 'contact', component: ContactComponent}
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'about', component: AboutComponent },
 ];
